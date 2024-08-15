f2fe429bc4631e0c6bdb9a71b96f005b83591bfc
Haxel Caribou <haxelcaribou@proton.me>
2024-08-14 19:06:27 -0700

    Import Haskeline to improve CLI usability

--- DiceRoller.hs
+++ DiceRoller.hs
@@ -1,6 +1,7 @@
 import Error
 import Evaluator
 import Parser
+import System.Console.Haskeline
 import System.IO (hFlush, stdout)
 import System.Random
 import Tokenizer
@@ -19,24 +20,20 @@ calcToString (Right n) = show n
 getAnswer :: String -> StdGen -> String
 getAnswer gen input = calcToString $ calc gen input
 
-showAnswer :: String -> StdGen -> IO()
-showAnswer input gen = do
-      putStrLn $ getAnswer input gen
-      newGen <- newStdGen
-      inputLoop input newGen
-
-inputLoop :: String -> StdGen -> IO ()
-inputLoop pInput gen = do
-  putStr "Enter Value: "
-  hFlush stdout
-  input <- getLine
-  case input of
-    "exit" -> putStrLn "exiting"
-    "quit" -> putStrLn "exiting"
-    "" -> showAnswer pInput gen
-    _ -> showAnswer input gen
-
 main :: IO ()
 main = do
   gen <- getStdGen
-  inputLoop "" gen
+  runInputT (setComplete noCompletion defaultSettings)  (loop gen)
+  where
+    loop :: StdGen -> InputT IO ()
+    loop gen = do
+      minput <- getInputLine "Enter Value: "
+      case minput of
+        Nothing -> return ()
+        Just "" -> loop gen
+        Just "exit" -> return ()
+        Just "quit" -> return ()
+        Just input -> do
+          outputStrLn $ getAnswer input gen
+          newGen <- newStdGen
+          loop newGen
