c422cce7dd7f1de4d010a749c2cab8e45bf8ede7
Haxel Caribou <haxelcaribou@proton.me>
2024-08-21 16:18:41 -0700

    Command line options‽ How incredible!

--- DiceRoller.hs
+++ DiceRoller.hs
@@ -1,6 +1,7 @@
 import Data.Char (toLower)
 import Error
 import Evaluator
+import Options.Applicative
 import Parser
 import System.Console.ANSI
 import System.Console.Haskeline
@@ -17,53 +18,98 @@ import Tokenizer
 calc :: String -> StdGen -> ErrorProne Double
 calc = evaluate . parse . tokenize
 
-calcToString :: ErrorProne Double -> String
-calcToString (Left e) = "error: " ++ e
-calcToString (Right n) = styleText [SetConsoleIntensity BoldIntensity] $ show n
+calcToString :: Options -> ErrorProne Double -> String
+calcToString o (Left e) = applyIfTrue (styleText [SetColor Foreground Dull Red]) (not $ noColor o) (applyIfTrue (styleText [SetConsoleIntensity BoldIntensity]) (bold o) "error: ") ++ e
+calcToString o (Right n) = applyIfTrue (styleText [SetConsoleIntensity BoldIntensity]) (bold o) $ show n
 
-getAnswer :: String -> StdGen -> String
-getAnswer gen input = calcToString $ calc gen input
+getAnswer :: Options -> String -> StdGen -> String
+getAnswer o gen input = calcToString o $ calc gen input
 
 styleText :: [SGR] -> String -> String
 styleText style text = setSGRCode style ++ text ++ setSGRCode [Reset]
 
-interactive :: IO ()
-interactive = do
+applyIfTrue :: (a -> a) -> Bool -> a -> a
+applyIfTrue f True x = f x
+applyIfTrue _ False x = x
+
+interactive :: Options -> IO ()
+interactive o = do
   gen <- getStdGen
-  runInputT (setComplete noCompletion defaultSettings) (loop gen)
+  runInputT (setComplete noCompletion defaultSettings) (loop o gen)
   where
-    loop :: StdGen -> InputT IO ()
-    loop gen = do
-      minput <- getInputLine $ styleText [SetColor Foreground Vivid Blue] "Enter Value: "
+    loop :: Options -> StdGen -> InputT IO ()
+    loop o gen = do
+      minput <- getInputLine $ applyIfTrue (styleText [SetColor Foreground Vivid Blue]) (not $ noColor o) "Enter Value: "
       case minput of
         Nothing -> return ()
-        Just input -> takeInput gen $ map toLower input
+        Just input -> takeInput o gen $ map toLower input
 
-    takeInput :: StdGen -> String -> InputT IO ()
-    takeInput gen input = case input of
-      "" -> loop gen
+    takeInput :: Options -> StdGen -> String -> InputT IO ()
+    takeInput o gen input = case input of
+      "" -> loop o gen
       "exit" -> return ()
       "quit" -> return ()
       "t" -> do
-        outputStrLn $ getAnswer "1d20" gen
+        outputStrLn $ getAnswer o "1d20" gen
         newGen <- newStdGen
-        loop newGen
+        loop o newGen
       "a" -> do
-        outputStrLn $ getAnswer "2d20b1" gen
+        outputStrLn $ getAnswer o "2d20b1" gen
         newGen <- newStdGen
-        loop newGen
+        loop o newGen
       "d" -> do
-        outputStrLn $ getAnswer "2d20t1" gen
+        outputStrLn $ getAnswer o "2d20t1" gen
         newGen <- newStdGen
-        loop newGen
+        loop o newGen
       "s" -> do
-        outputStrLn $ getAnswer "4d6b1" gen
+        outputStrLn $ getAnswer o "4d6b1" gen
         newGen <- newStdGen
-        loop newGen
+        loop o newGen
       input -> do
-        outputStrLn $ getAnswer input gen
+        outputStrLn $ getAnswer o input gen
         newGen <- newStdGen
-        loop newGen
+        loop o newGen
+
+data Options = Options
+  { noColor :: Bool,
+    bold :: Bool,
+    clear :: Bool
+  }
+
+options :: Parser Options
+options =
+  Options
+    <$> switch
+      ( long "no-color"
+          <> short 'n'
+          <> help "Don't color output"
+      )
+    <*> switch
+      ( long "bold"
+          <> short 'b'
+          <> help "Output final answers in bold"
+      )
+    <*> switch
+      ( long "clear"
+          <> short 'c'
+          <> help "Clear screen before and after running"
+      )
 
 main :: IO ()
-main = interactive
+main = parseArgs =<< execParser opts
+  where
+    opts =
+      info
+        (options <**> helper)
+        ( fullDesc
+            <> progDesc "Roll dice and do math"
+        )
+
+parseArgs :: Options -> IO ()
+parseArgs (Options noColor bold True) = do
+  clearScreen
+  setCursorPosition 0 0
+  parseArgs (Options noColor bold False)
+  clearScreen
+  setCursorPosition 0 0
+parseArgs options@(Options noColor bold False) = interactive options
