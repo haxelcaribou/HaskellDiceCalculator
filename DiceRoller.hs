9fc1e762e537fccd8fbec903662f098e1ab0ade8
Haxel Caribou <haxelcaribou@proton.me>
2024-08-21 16:30:22 -0700

    Let's format this up and make it look nice and pretty

--- DiceRoller.hs
+++ DiceRoller.hs
@@ -15,22 +15,41 @@ import Tokenizer
 --  pecentge dice (d%)
 --  repeated rolls ([n]x[roll])
 
+data Options = Options
+  { noColor :: Bool,
+    bold :: Bool,
+    clear :: Bool
+  }
+
+applyIfTrue :: (a -> a) -> Bool -> a -> a
+applyIfTrue f True x = f x
+applyIfTrue _ False x = x
+
+boldText :: Options -> String -> String
+boldText o =
+  applyIfTrue
+    (styleText [SetConsoleIntensity BoldIntensity])
+    (bold o)
+
+colorText :: Options -> ColorIntensity -> Color -> String -> String
+colorText o i c =
+  applyIfTrue
+    (styleText [SetColor Foreground i c])
+    (not $ noColor o)
+
 calc :: String -> StdGen -> ErrorProne Double
 calc = evaluate . parse . tokenize
 
 calcToString :: Options -> ErrorProne Double -> String
-calcToString o (Left e) = applyIfTrue (styleText [SetColor Foreground Dull Red]) (not $ noColor o) (applyIfTrue (styleText [SetConsoleIntensity BoldIntensity]) (bold o) "error: ") ++ e
-calcToString o (Right n) = applyIfTrue (styleText [SetConsoleIntensity BoldIntensity]) (bold o) $ show n
+calcToString o (Left e) =
+  colorText o Dull Red (boldText o "error: ") ++ e
+calcToString o (Right n) = boldText o $ show n
 
 getAnswer :: Options -> String -> StdGen -> String
 getAnswer o gen input = calcToString o $ calc gen input
 
 styleText :: [SGR] -> String -> String
 styleText style text = setSGRCode style ++ text ++ setSGRCode [Reset]
-
-applyIfTrue :: (a -> a) -> Bool -> a -> a
-applyIfTrue f True x = f x
-applyIfTrue _ False x = x
 
 interactive :: Options -> IO ()
 interactive o = do
@@ -39,7 +58,8 @@ interactive o = do
   where
     loop :: Options -> StdGen -> InputT IO ()
     loop o gen = do
-      minput <- getInputLine $ applyIfTrue (styleText [SetColor Foreground Vivid Blue]) (not $ noColor o) "Enter Value: "
+      minput <-
+        getInputLine $ colorText o Vivid Blue "Enter Value: "
       case minput of
         Nothing -> return ()
         Just input -> takeInput o gen $ map toLower input
@@ -70,12 +90,6 @@ interactive o = do
         newGen <- newStdGen
         loop o newGen
 
-data Options = Options
-  { noColor :: Bool,
-    bold :: Bool,
-    clear :: Bool
-  }
-
 options :: Parser Options
 options =
   Options
@@ -95,6 +109,15 @@ options =
           <> help "Clear screen before and after running"
       )
 
+parseArgs :: Options -> IO ()
+parseArgs (Options noColor bold True) = do
+  clearScreen
+  setCursorPosition 0 0
+  parseArgs (Options noColor bold False)
+  clearScreen
+  setCursorPosition 0 0
+parseArgs options@(Options noColor bold False) = interactive options
+
 main :: IO ()
 main = parseArgs =<< execParser opts
   where
@@ -104,12 +127,3 @@ main = parseArgs =<< execParser opts
         ( fullDesc
             <> progDesc "Roll dice and do math"
         )
-
-parseArgs :: Options -> IO ()
-parseArgs (Options noColor bold True) = do
-  clearScreen
-  setCursorPosition 0 0
-  parseArgs (Options noColor bold False)
-  clearScreen
-  setCursorPosition 0 0
-parseArgs options@(Options noColor bold False) = interactive options
